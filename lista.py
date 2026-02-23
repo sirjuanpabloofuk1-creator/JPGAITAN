c563adf4461d78e2e3542c331880088e6db69d06
JUANPABLO <sirjuanpabloofuk1@gmail.com>
2026-02-23 11:37:48 -0500

    Agregado archivo lista.py con un saludo basico

--- lista.py
+++ lista.py
@@ -3,13 +3,12 @@
 echo "Ingrese 7 números separados por espacio:"
 read -a numeros
 
-# Verificar que sean 7 números
 if [ ${#numeros[@]} -ne 7 ]; then
     echo "Debe ingresar exactamente 7 números."
     exit 1
 fi
 
-# Ordenar los números
+
 ordenados=($(printf "%s\n" "${numeros[@]}" | sort -n))
 
 echo "Números ordenados de menor a mayor:"
